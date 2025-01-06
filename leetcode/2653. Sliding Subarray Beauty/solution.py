c377b83d9587f83beef86ce4aa960323f226ea13
James Totah <jtotah@usfca.edu>
2025-01-06 16:12:04 -0600

    Printing the subarrays for access

--- leetcode/2653. Sliding Subarray Beauty/solution.py
+++ leetcode/2653. Sliding Subarray Beauty/solution.py
@@ -1,3 +1,6 @@
 class Solution:
     def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
-        
+        for i in range(0, len(nums)):
+            if(i+k <= len(nums)):
+                for j in range(i, i+k):
+                    print(nums[j])
