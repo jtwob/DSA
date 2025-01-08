c5876431680a43d1775e0635cd7e6e44dd99058b
James Totah <jtotah@usfca.edu>
2025-01-07 18:41:36 -0600

    Passed test cases

--- leetcode/2653. Sliding Subarray Beauty/solution.py
+++ leetcode/2653. Sliding Subarray Beauty/solution.py
@@ -1,17 +1,15 @@
 class Solution:
     def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
-        beauties = [len(nums)-k+1]
-        for i in range(0, len(nums)):
-            negative_count = 0
-            lowest = 0
-            beauty = 0
-            if(i+k <= len(nums)):
-                for j in range(i, i+k):
-                    print(nums[j])
-                    if(nums[j] < 0):
-                        negative_count+=1
-                    if(nums[j] < lowest):
-                        beauty = lowest
-                        lowest = nums[j]
-                beauties.append(beauty)                   
+        beauties = []
+        for i in range(0, len(nums)-k+1):
+            negatives = []
+            for j in range(i, i+k):
+                print(nums[j])
+                if(nums[j] < 0):
+                    negatives.append(nums[j])
+            if(len(negatives) >= x):
+                negatives.sort()
+                beauties.append(negatives[x-1])
+            else:
+                beauties.append(0)                   
         return beauties
