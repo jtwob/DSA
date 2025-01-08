3d93c4e018531d4e356af406ab31bc894bba0cd7
James Totah <jtotah@usfca.edu>
2025-01-08 12:35:54 -0600

    Passes all tests

--- leetcode/841. Keys and Rooms/solution.py
+++ leetcode/841. Keys and Rooms/solution.py
@@ -4,12 +4,11 @@ class Solution:
         visited = []
         while len(queue) != 0:
             newRoom = queue.pop()
-            if(newRoom in visited):
-                return False
-            nextRooms = rooms[newRoom]
-            visited.append(newRoom)
-            for i in range(len(nextRooms)):
-                queue.append(nextRooms[i])
+            if(newRoom not in visited):     
+                nextRooms = rooms[newRoom]
+                visited.append(newRoom)
+                for i in range(len(nextRooms)):
+                    queue.append(nextRooms[i])
             
         if len(visited) < len(rooms):
             return False
