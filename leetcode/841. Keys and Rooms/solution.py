5c43306c13e094333e3395a49cf8325f769eb757
James Totah <jtotah@usfca.edu>
2025-01-08 12:32:26 -0600

    Passes prelim test cases

--- leetcode/841. Keys and Rooms/solution.py
+++ leetcode/841. Keys and Rooms/solution.py
@@ -1,3 +1,17 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        return False
+        queue = [0]
+        visited = []
+        while len(queue) != 0:
+            newRoom = queue.pop()
+            if(newRoom in visited):
+                return False
+            nextRooms = rooms[newRoom]
+            visited.append(newRoom)
+            for i in range(len(nextRooms)):
+                queue.append(nextRooms[i])
+            
+        if len(visited) < len(rooms):
+            return False
+        else:
+            return True
