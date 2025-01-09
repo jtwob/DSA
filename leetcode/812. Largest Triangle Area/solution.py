25f49f352908d2115848ca9bf4fdf45181b81ba5
James Totah <jtotah@usfca.edu>
2025-01-09 16:17:53 -0600

    Triangle area calculator works

--- leetcode/812. Largest Triangle Area/solution.py
+++ leetcode/812. Largest Triangle Area/solution.py
@@ -7,13 +7,19 @@ class Solution:
         y1 = points[0][1]
         y2 = points[1][1]
         y3 = points[2][1]
+        m_base = 0
+        m_height = 0
+        x_intercept = 0
 
         base = pow(pow(x1-x2, 2) + pow(y1-y2, 2), 0.5)
-        m_base = (y1-y2)/(x1-x2)
+        if x1 != 0 and x2 != 0:
+            m_base = (y1-y2)/(x1-x2)
         b_base = y1 - m_base * x1
-        m_height = -1/m_base
+        if m_base != 0:
+            m_height = -1/m_base
         b_height = y3 - m_height * x3
-        x_intercept = (b_height - b_base) / (m_base - m_height)
+        if m_base != m_height:
+            x_intercept = (b_height - b_base) / (m_base - m_height)
         y_intercept = m_base * x_intercept + b_base
 
         h = pow(pow(x_intercept-x3, 2) + pow(y_intercept-y3, 2), 0.5)
