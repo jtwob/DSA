4b436ca33cadc79a2d4f27c5d407ea75611b65b4
James Totah <jtotah@usfca.edu>
2025-01-09 15:14:46 -0600

    Added rough idea algorithm solution

--- leetcode/812. Largest Triangle Area/solution.py
+++ leetcode/812. Largest Triangle Area/solution.py
@@ -1,3 +1,6 @@
 class Solution:
     def largestTriangleArea(self, points: List[List[int]]) -> float:
+        # Prelim algorithm
+        # Recursively calculate areas from all points
+        # Return max
         return 0
