aceba840b0f002eea0109d20960a023725bc1008
James Totah <jtotah@usfca.edu>
2025-01-09 16:52:00 -0600

    passing 56/57 tests

--- leetcode/812. Largest Triangle Area/solution.py
+++ leetcode/812. Largest Triangle Area/solution.py
@@ -14,13 +14,19 @@ class Solution:
         base = pow(pow(x1-x2, 2) + pow(y1-y2, 2), 0.5)
         if x1 != x2:
             m_base = (y1-y2)/(x1-x2)
+        else:
+            m_height = 0
         b_base = y1 - m_base * x1
         if m_base != 0:
             m_height = -1/m_base
         b_height = y3 - m_height * x3
-        if m_base != m_height:
-            x_intercept = (b_height - b_base) / (m_base - m_height)
-        y_intercept = m_base * x_intercept + b_base
+        if x1 == x2:
+            x_intercept = x1
+            y_intercept = y3
+        else:
+            if m_base != m_height:
+                x_intercept = (b_height - b_base) / (m_base - m_height)
+            y_intercept = m_base * x_intercept + b_base
 
         h = pow(pow(x_intercept-x3, 2) + pow(y_intercept-y3, 2), 0.5)
 
@@ -35,8 +41,8 @@ class Solution:
 
         max = 0
 
-        for i in range(len(points)):
-            for j in range(i, len(points)):
+        for i in range(len(points)-2):
+            for j in range(i, len(points)-1):
                 for k in range(j, len(points)):
                     area = self.calcArea([points[i], points[j], points[k]])
                     if area > max:
