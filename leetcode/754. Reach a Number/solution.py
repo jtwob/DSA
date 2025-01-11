a22ba1870b658d07dbec3aa955f96dcb8c0aafe2
James Totah <jtotah@usfca.edu>
2025-01-10 18:04:33 -0600

    Added strategy todo

--- leetcode/754. Reach a Number/solution.py
+++ leetcode/754. Reach a Number/solution.py
@@ -1,3 +1,5 @@
 class Solution:
     def reachNumber(self, target: int) -> int:
+        # Strategy
+        # a solution can be found by moving, determining the distance to target, and determining the distance to the target on the next branch either + or -
         return 0
