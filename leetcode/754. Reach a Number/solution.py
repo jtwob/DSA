fa19e58506822acc34d4a7bdf4b3093ee1a1cd7e
James Totah <jtotah@usfca.edu>
2025-01-11 22:27:07 -0600

    Gotta start

--- leetcode/754. Reach a Number/solution.py
+++ leetcode/754. Reach a Number/solution.py
@@ -2,4 +2,5 @@ class Solution:
     def reachNumber(self, target: int) -> int:
         # Strategy
         # a solution can be found by moving, determining the distance to target, and determining the distance to the target on the next branch either + or -
+        stepCount = 0
         return 0
