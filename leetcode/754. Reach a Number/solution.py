4532e7b45fd77c3993c523146044b11e85fa204b
James Totah <jtotah@usfca.edu>
2025-01-16 20:44:11 -0600

    First try. Algo incorrect

--- leetcode/754. Reach a Number/solution.py
+++ leetcode/754. Reach a Number/solution.py
@@ -2,5 +2,12 @@ class Solution:
     def reachNumber(self, target: int) -> int:
         # Strategy
         # a solution can be found by moving, determining the distance to target, and determining the distance to the target on the next branch either + or -
-        stepCount = 0
-        return 0
+        stepCount = 1
+        location = 0
+        while(location != target):
+            if(location < target):
+                location += stepCount
+            elif(location > target):
+                location -= target
+            stepCount += 1
+        return stepCount
