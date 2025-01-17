8588ffcdbf2ea5500871d4a7cb8b00b19ccd5f13
James Totah <jtotah@usfca.edu>
2025-01-16 20:47:58 -0600

    Time limit exceeded but passed 31/73 testcases

--- leetcode/754. Reach a Number/solution.py
+++ leetcode/754. Reach a Number/solution.py
@@ -2,12 +2,19 @@ class Solution:
     def reachNumber(self, target: int) -> int:
         # Strategy
         # a solution can be found by moving, determining the distance to target, and determining the distance to the target on the next branch either + or -
-        stepCount = 1
-        location = 0
-        while(location != target):
-            if(location < target):
-                location += stepCount
-            elif(location > target):
-                location -= target
-            stepCount += 1
-        return stepCount
+        queue = [0]
+        step = 1
+        while queue:
+            curr_level = len(queue)
+            for i in range(curr_level):
+                current = queue.pop(0)
+                go_left = current - step
+                go_right = current + step
+                
+                if go_left == target or go_right == target:
+                    return step
+                
+                queue += [go_left, go_right]
+            step += 1
+
+        return -1
